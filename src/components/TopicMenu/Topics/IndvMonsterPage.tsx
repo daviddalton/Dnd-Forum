219a11f19059a0f6ef8a157b2615e56c1b39625c
Jared Williams <Jared.Williams101697@gmail.com>
2023-03-01 14:18:38 -0500

    added monster ability scores

--- src/components/TopicMenu/Topics/IndvMonsterPage.tsx
+++ src/components/TopicMenu/Topics/IndvMonsterPage.tsx
@@ -2,12 +2,18 @@ import { useParams } from "react-router-dom";
 import Monster from "../../../model/Character/Monster";
 import React, { useEffect, useState } from "react"
 import axios from 'axios';
+import { AbilityScore } from "../../../model/Character/AbilityScore";
+
 
 
 function IndvMonsterPage(props: any) {
 
     const { monsterSlug } = useParams()
     const [monsterData, setMonsterData] = useState<Monster>()
+    const abilityShortNames = ['STR', 'DEX', 'CON', 'INT', 'WIS', 'CHA']
+    const monsterAbilityScores = [monsterData?.strength, monsterData?.dexterity, monsterData?.constitution, monsterData?.intelligence, monsterData?.wisdom, monsterData?.charisma]
+    const [abilityScores, setAbilityScores] = useState<AbilityScore[]>([])
+
 
     React.useEffect(() => {
         axios.get(`https://api.open5e.com/monsters/${monsterSlug}`)
@@ -18,8 +24,21 @@ function IndvMonsterPage(props: any) {
                 console.error(error)
             })
     }, [monsterSlug])
+
+    React.useEffect(() => {
+        if (monsterData) {
+            var tempAbilityArray: AbilityScore[] = []
+            for(let i = 0; i < abilityShortNames.length; i++) {
+                let tempAbilityScore = new AbilityScore(abilityShortNames[i], monsterAbilityScores[i]!.toString(),[])
+                tempAbilityScore.modifier = tempAbilityScore.findModifier()
+                tempAbilityArray.push(tempAbilityScore)
+            }
+            setAbilityScores(tempAbilityArray)
+        }
+
+    }, [monsterData])
     
-    console.log(monsterData)
+    console.log(abilityScores)
     return (
         <div
             style={{
@@ -49,6 +68,14 @@ function IndvMonsterPage(props: any) {
                             monsterHitPoints={monsterData?.hit_points}
                             monsterHitPointsDesc={monsterData?.hit_dice}
                             monsterSpeed={monsterData?.speed} />
+                        <MonsterAbilityScores 
+                            monsterStrength={monsterData?.strength}
+                            monsterDexterity={monsterData?.dexterity}
+                            monsterConstitution={monsterData?.constitution}
+                            monsterIntelligence={monsterData?.intelligence}
+                            monsterWisdom={monsterData?.wisdom}
+                            monsterCharisma={monsterData?.charisma}
+                            abilityScores={abilityScores}/>
                 </div>
         </div>
     )
@@ -105,10 +132,49 @@ function MonsterArmorHPSpeed(props: any) {
                         display: 'flex'
                     }}>
                     <strong>Speed:</strong> 
-                    <div style={{ paddingLeft: '5px'}}>{props.monsterSpeed.burrow !== undefined ? (<span> Burrow - {props.monsterSpeed.burrow} ft.</span>):(<div></div>)}</div>
-                    <div style={{ paddingLeft: '5px'}}>{props.monsterSpeed.swim !== undefined ? (<span> Swim - {props.monsterSpeed.swim} ft.</span>):(<div></div>)}</div>
-                    <div style={{ paddingLeft: '5px'}}>{props.monsterSpeed.walk !== undefined ? (<span> Walk - {props.monsterSpeed.walk} ft.</span>):(<div></div>)}</div>
+                    <div style={{ paddingLeft: '5px'}}>{props.monsterSpeed?.burrow !== undefined ? (<span> Burrow - {props.monsterSpeed?.burrow} ft.</span>):(<div></div>)}</div>
+                    <div style={{ paddingLeft: '5px'}}>{props.monsterSpeed?.swim !== undefined ? (<span> Swim - {props.monsterSpeed?.swim} ft.</span>):(<div></div>)}</div>
+                    <div style={{ paddingLeft: '5px'}}>{props.monsterSpeed?.walk !== undefined ? (<span> Walk - {props.monsterSpeed?.walk} ft.</span>):(<div></div>)}</div>
                 </div>
+        </div>
+    )
+}
+
+function MonsterAbilityScores(props: any) {
+    return (
+        <div
+            style={{
+                border: '1px white solid',
+                margin: '5px',
+                display: 'flex',
+                justifyContent: "space-evenly"
+            }}>
+                {props.abilityScores.map((abilityScore: AbilityScore) => (
+                <div
+                style={{
+                    border: '1px white solid',
+                }}>
+                    <div
+                        style={{
+                            border: '1px white solid',
+                            margin: '5px',
+                            display: 'flex',
+                            justifyContent: "center"
+                        }}>
+                            <strong>{abilityScore.scoreName}</strong>
+                    </div>
+                    <div
+                        style={{
+                            border: '1px white solid',
+                            margin: '5px',
+                            display: 'flex',
+                            justifyContent: "center"
+                        }}>
+                            {abilityScore.total} ({abilityScore.modifier})
+                    </div>
+                </div>
+                ))}
+
         </div>
     )
 }
