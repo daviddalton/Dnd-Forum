d3e2b919679e2f1c7a4276424cbe4343cd8aab6a
Jared Williams <Jared.Williams101697@gmail.com>
2023-03-01 13:17:52 -0500

    added size, type, alignment, armor class, hp, and speed to monster page

--- src/components/TopicMenu/Topics/IndvMonsterPage.tsx
+++ src/components/TopicMenu/Topics/IndvMonsterPage.tsx
@@ -19,25 +19,99 @@ function IndvMonsterPage(props: any) {
             })
     }, [monsterSlug])
     
-
+    console.log(monsterData)
     return (
         <div
             style={{
                 marginTop: '20px',
                 border: '1px white solid',
-                width: '100%'
+                width: '100%',
+                color: 'white',
+                display: 'flex',
+                justifyContent: 'center'
             }}>
                 <div
                     style={{
                         border: '1px white solid',
-                        margin: '5px'
+                        margin: '5px',
+                        minWidth: '320px',
+                        maxWidth: '780px',
+                        width: '100%',
+                        opacity: '.6'
                     }}>
-
+                        <MonsterTitle monsterName={monsterData?.name} />
+                        <MonsterSizeTypeAlignment 
+                            monsterSize={monsterData?.size} 
+                            monsterType={monsterData?.type}
+                            monsterAlignment={monsterData?.alignment} />
+                        <MonsterArmorHPSpeed 
+                            monsterArmor={monsterData?.armor_class}  
+                            monsterHitPoints={monsterData?.hit_points}
+                            monsterHitPointsDesc={monsterData?.hit_dice}
+                            monsterSpeed={monsterData?.speed} />
                 </div>
         </div>
     )
 }
 
+function MonsterTitle(props: any) {
+    return (
+        <div
+            style={{
+                border: '1px white solid',
+                margin: '5px',
+                display: 'flex',
+
+                background: '#761e21'
+            }}>
+                <h1>{props.monsterName}</h1>
+        </div>
+    )
+}
+
+function MonsterSizeTypeAlignment(props: any) {
+    return (
+        <>
+        <div style={{ borderTop: '1px white solid' }} />
+        <div
+            style={{
+                border: '1px white solid',
+                margin: '5px',
+                display: 'flex',
+            }}>
+                {props.monsterSize} | {props.monsterType} | {props.monsterAlignment}
+        </div>
+        </>
+    )
+}
+
+function MonsterArmorHPSpeed(props: any) {
+    return (
+        <div
+            style={{
+                border: '1px white solid',
+                display: 'flex',
+                margin: '5px',
+                flexDirection: "column"
+            }}>
+                <div>
+                    <strong>Armor Class: </strong>{props.monsterArmor}
+                </div>
+                <div>
+                    <strong>Hit Points: </strong>{props.monsterHitPoints} ({props.monsterHitPointsDesc})
+                </div>
+                <div
+                    style={{
+                        display: 'flex'
+                    }}>
+                    <strong>Speed:</strong> 
+                    <div style={{ paddingLeft: '5px'}}>{props.monsterSpeed.burrow !== undefined ? (<span> Burrow - {props.monsterSpeed.burrow} ft.</span>):(<div></div>)}</div>
+                    <div style={{ paddingLeft: '5px'}}>{props.monsterSpeed.swim !== undefined ? (<span> Swim - {props.monsterSpeed.swim} ft.</span>):(<div></div>)}</div>
+                    <div style={{ paddingLeft: '5px'}}>{props.monsterSpeed.walk !== undefined ? (<span> Walk - {props.monsterSpeed.walk} ft.</span>):(<div></div>)}</div>
+                </div>
+        </div>
+    )
+}
 export default IndvMonsterPage
 
 
